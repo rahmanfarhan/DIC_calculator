9f3940825a7ec361aa870d28067c39c679e1b8ee
FarhanPersonal <frn.rahman@gmail.com>
2022-08-13 01:09:59 -0600

    Updated code taking 2 numbers and showing sum

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 document.querySelector("button").addEventListener("click", handleClick)
 
 function handleClick(){
-  alert("I got clicked.");
+  let num_1 = document.getElementById("num_1").value;
+  let num_2 = document.getElementById("num_2").value;
+  let sum =  parseInt(num_1)+parseInt(num_2);
+  alert("Sum of number 1 and number 2 are: " + sum);
 }
